05899b0ed033fe63c2341f8c05a5081ac4eb7a5f
arjun-reboostify <reboostify@gmail.com>
2025-01-31 12:33:36 +0530

    F

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,9 @@
-
+import FaceLandmarker from '@/components/face-landmarks';
 
 export default function Home() {
-  return (
-    <div className="text">
-      <h1>PRANAV</h1>
-      <h1>ABHIMANYU</h1>
-      <h1>ARJUN</h1>
-  
-      </div>
-  );
+	return (
+		<>
+			<FaceLandmarker />
+		</>
+	);
 }
